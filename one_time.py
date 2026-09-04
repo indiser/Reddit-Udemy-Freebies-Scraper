0a38846f7427b2cb1fa28d7fa80a9daced2f131f
indiser <banerjeerana74@gmail.com>
2026-09-04 10:42:31 +0530

    Refactor scraper to use session with cookies and headers

--- one_time.py
+++ one_time.py
@@ -1,7 +1,12 @@
-import requests
+from curl_cffi import requests
 import json
 import html
 import re
+
+with open("cookies.json", "r", encoding="utf-8") as filp:
+    raw_cookies = json.load(filp)
+
+COOKIES = {cookie['name'] : cookie['value'] for cookie in raw_cookies}
 
 PATTERN = re.compile(
     r"(100.*days.*code.*angela.*yu)|"  # Case 1: Course Name ... Instructor
@@ -12,11 +17,21 @@ PATTERN = re.compile(
 
 reddit_url="https://www.reddit.com/r/udemyfreeebies/.json"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
 }
 
-response=requests.get(url=reddit_url,headers=headers)
+session = requests.Session(impersonate="chrome120")
+session.cookies.update(COOKIES)
+session.headers.update(HEADERS)
+
+# response=requests.get(url=reddit_url,impersonate="chrome120", cookies=COOKIES, headers=HEADERS)
+response = session.get(reddit_url)
 
 data=html.unescape(response.json()["data"]["children"][1]["data"]["selftext"])
 
