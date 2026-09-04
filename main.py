01a1062cacdb76ab9020683d5a54ed9042d4ce98
indiser <banerjeerana74@gmail.com>
2026-09-04 10:42:11 +0530

    Refactor Reddit scraper to use session and cookies

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import requests
+from curl_cffi import requests
 import html
 import re
-import time
+import json
 
 # 1. CONSTANTS
-SUBREDDIT_URL = "https://www.reddit.com/r/udemyfreeebies/.json?limit=5"
+limit=10
+SUBREDDIT_URL = f"https://www.reddit.com/r/udemyfreeebies/.json?limit={limit}"
+
+with open("cookies.json", "r", encoding="utf-8") as filp:
+    raw_cookies = json.load(filp)
+
+COOKIES = {cookie['name'] : cookie['value'] for cookie in raw_cookies}
 
 PATTERN = re.compile(
     r"(100.*days.*code.*angela.*yu)|"  # Case 1: Course Name ... Instructor
@@ -13,14 +19,23 @@ PATTERN = re.compile(
     re.IGNORECASE
 )
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
 }
+
+session = requests.Session(impersonate="chrome120")
+session.cookies.update(COOKIES)
+session.headers.update(HEADERS)
 
 print("--- Scraper Hit Verification ---")
 try:
     print(f"Scraping {SUBREDDIT_URL}...")
-    response = requests.get(url=SUBREDDIT_URL, headers=headers)
+    response = session.get(SUBREDDIT_URL)
     
     if response.status_code == 429:
         raise Exception("Blocked by Reddit (Too Many Requests). Wait a while.")
@@ -40,6 +55,8 @@ try:
         selftext = html.unescape(post_data.get("selftext", ""))
         full_text = f"{title} \n {selftext}"
 
+        print(f"• {title}")
+
         if PATTERN.search(full_text):
             print("\n" + "="*40)
             print(f"🔥 MATCH FOUND!")
@@ -49,7 +66,7 @@ try:
             found_count += 1
 
     if found_count == 0:
-        print("\nNo matches found in the top 5 posts.")
+        print(f"\nNo matches found in the top {limit} posts.")
 
 except Exception as e:
     print(f"CRITICAL FAILURE: {e}")
